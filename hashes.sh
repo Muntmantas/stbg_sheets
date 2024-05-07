5060a957a283c69721c2d5531ebbd1d8270a0b17
Muntmantas <77965496+Muntmantas@users.noreply.github.com>
2024-05-07 18:51:32 +0300

    Create hashes.sh

---
+++ hashes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -A hashes
+
+hashes["breaks"]="Breaks"
+hashes["drum-bass"]="Drum and Bass"
+hashes["funk"]="Funk"
+hashes["l-etincelle"]="L'etincelle"
+hashes["rope-skipping"]="Rope Skipping"
+hashes["tune-ak"]="Tune Ak"
+hashes["love-and-rage"]="Love and Rage"
